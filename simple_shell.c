31d6d6f77770d54d31de2431b44571f6673eda7c
Jason Dias <dias.jason9@gmail.com>
2017-01-24 10:35:55 -0500

    * Added typdef struct job and maintain list of jobs * Some refactoring, to allow access to user input in execute

--- simple_shell.c
+++ simple_shell.c
@@ -5,12 +5,23 @@
 #include <sys/wait.h>
 
 #define ARG_MAX 100 /* Max char length in sh is actually 2097152. Reduced
-                        to reduce impact */
+                        to reduced here for simplification */
+#define MAX_BG 10 
 
-int getcmd(char *prompt, char *args[], int *background) {
+int BG_COUNT = 0;
+
+
+typedef struct job {
+    pid_t pid;
+    char cmd[ARG_MAX];
+
+} job;
+
+struct job jobs[MAX_BG];
+
+int getcmd(char *prompt, char *args[], int *background, char *line) {
     int length, i = 0;
     char *token, *loc;
-    char *line = NULL;
     size_t linecap = 0;
 
     printf("%s", prompt);
@@ -40,7 +51,7 @@ int getcmd(char *prompt, char *args[], int *background) {
     return i;
 }
 
-int execute(char *args[], int bg) {
+int execute(char *line, char *args[], int bg) {
     pid_t pid = fork();
     int status;
     if(pid < 0) {
@@ -54,20 +65,26 @@ int execute(char *args[], int bg) {
     }
     if(!bg) {
         while(wait(&status) != pid);
-        printf("%i", (int)pid);
+    } else {
+        printf("[%i]  + %i", BG_COUNT + 1, (int)pid);    
+        jobs[BG_COUNT].pid = pid;
+        strcpy(jobs[BG_COUNT].cmd, line);
     }
 }
 
+int listjobs() {
 
+}
 
 int main(void) {
     
     char *args[20];
+    char *line;
     int bg;
 
     while(1) {
         bg = 0;
-        int cnt = getcmd("\njsh >> ", args, &bg);
+        int cnt = getcmd("\njsh >> ", args, &bg, line);
         if(strcmp(args[0], "pwd") == 0) {
             char path[ARG_MAX];
             if(getcwd(path, ARG_MAX) != NULL) {
@@ -80,11 +97,11 @@ int main(void) {
         } else if(strcmp(args[0], "exit") == 0) {
             exit(0);
         } else if(strcmp(args[0], "fg") == 0) {
-            pid_t pid = jobs[atoi(args[1]) + 1].pid; //TODO : maintain jobs ds
-            waitpid(pid, NULL, 0); /* bring to the front */
+            pid_t pid = jobs[atoi(args[1]) + 1].pid;
+            waitpid(pid, NULL, 0);
         } else if(strcmp(args[0], "jobs") == 0) {
-           listJobs(); //TODO : implement helper func to list jobs
+           listjobs(); //TODO : implement helper func to list jobs
         }
-        execute(args, bg);
+        execute(line, args, bg);
     }
 }
