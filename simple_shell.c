7848054003be2b3919571c2ec7b493f623fbd59f
Jason Dias <dias.jason9@gmail.com>
2017-01-31 11:52:07 -0500

    * Extracted logic to helper methods * Cleaned up and added comments

--- simple_shell.c
+++ simple_shell.c
@@ -11,11 +11,10 @@
 
 
 int BG_COUNT = 0;
-
 typedef struct job {
     int jid;
     pid_t pid;
-    int state;
+    char state[10];
     char cmd[ARG_MAX];
 
 } job;
@@ -79,6 +78,7 @@ int get_cmd(char *args[], int *background, int *out, int *pip, int length, char 
     return i;
 }
 
+/* Provide a slice function for array generation in piping */
 int slice_array(char *args[], char *args_c[], int start, int end) {
     int i;
     for(i = 0; i+start < end; i++) {
@@ -90,6 +90,33 @@ int slice_array(char *args[], char *args_c[], int start, int end) {
     args_c[i+1] = NULL;
 }
 
+/* Execute piping within a helper execute method */
+int execute_pipe(char *args[], int cnt) {
+    int pipefd[2];
+    if(pipe(pipefd) == -1) {
+        perror("piping error");
+    }
+    pid_t pid = fork();
+    if(pid == 0) {
+        char *args1[20];
+        slice_array(args, args1, 0, cnt);
+        close(pipefd[0]);
+        dup2(pipefd[1], fileno(stdout));
+        close(pipefd[1]);
+        execvp(args1[0], args1);
+        perror("exec error: ");
+        exit(1);
+    }
+    char *args2[20];
+    slice_array(args, args2, cnt, 19);
+    close(pipefd[1]);
+    dup2(pipefd[0], fileno(stdin));
+    close(pipefd[0]);
+    execvp(args2[0], args2);
+    perror("exec error: ");
+    exit(1);
+}
+
 /* Execute the cmd and arguments */
 int execute(char *args[], int cnt, int bg, int out, int pip, char *line) {
     pid_t pid = fork();
@@ -98,64 +125,41 @@ int execute(char *args[], int cnt, int bg, int out, int pip, char *line) {
         printf("**** ERROR: fork failed\n");
         exit(1);
     } else if(pid == 0) {
+        //Child
         if(out) {
             close(fileno(stdout));
             int fd1;
             fd1 = open(args[cnt-1], O_RDWR|O_CREAT, 0777);
             args[cnt-1] = 0; 
         } else if(pip) {
-            int pipefd[2];
-            if(pipe(pipefd) == -1) {
-                perror("piping error");
-            }
-            pid_t pid = fork();
-            if(pid == 0) {
-                char *args1[20];
-                slice_array(args, args1, 0, cnt);
-                close(pipefd[0]);
-                dup2(pipefd[1], fileno(stdout));
-                close(pipefd[1]);
-                execvp(args1[0], args1);
-                perror("exec error: ");
-                exit(1);
-            }
-            char *args2[20];
-            slice_array(args, args2, cnt, 19);
-            close(pipefd[1]);
-            dup2(pipefd[0], fileno(stdin));
-            close(pipefd[0]);
-            execvp(args2[0], args2);
-            perror("exec error: ");
-            exit(1);
-        } 
-        
+           execute_pipe(args, cnt);
+        }
         if(execvp(args[0], args) < 0) { 
             printf("jsh: command not found :  %s\n", args[0]);//assumumption
             exit(1);
         } 
         fflush(stdout);
     }
-
+    //Parent
     if(!bg) {
         while(wait(&status) != pid);
     } else {
        struct job n_job;
        n_job.pid = pid;
        strcpy(n_job.cmd, line);
+       strcpy(n_job.state, "running");
        n_job.jid = BG_COUNT+1;
        jobs[BG_COUNT] = n_job;
        BG_COUNT++;
     }
-
     return 0;
 }
-
 
 /* List background jobs */
 int list_jobs() {
     int i;
     for(i = 0; i < BG_COUNT; i++) {
-        printf("[%i]  +  %s\n", jobs[i].jid, jobs[i].cmd);
+        printf("[%i]   %s  %s\n", jobs[i].jid, jobs[i].state, jobs[i].cmd);
     } 
 }
 
@@ -169,11 +173,31 @@ int merge_cmd(char **cmd, char *args[], int size) {
     return 0;
 }
 
+/*Check if the cmd is inbuilt, and execute*/
+int check_exec_built_in(char *args[]) {
+    if(strcmp(args[0], "pwd") == 0) {
+        char path[ARG_MAX];
+        if(getcwd(path, ARG_MAX) != NULL) {
+            printf("%s\n", path);
+            return 1;
+        }
+    } else if(strcmp(args[0], "cd") == 0) {
+        chdir(args[1]);
+        return 1;
+    } else if(strcmp(args[0], "exit") == 0) {
+        exit(0);
+    } else if(strcmp(args[0], "fg") == 0) {
+        pid_t pid = jobs[atoi(args[1]) + 1].pid;
+        waitpid(pid, NULL, 0);
+        return 1;
+    } else if(strcmp(args[0], "jobs") == 0) { 
+        list_jobs();
+        return 1;
+    } 
+}
 
 /*Main Routine*/
-
-int main(void) {
-    
+int main(void) { 
     char *args[20];
     int bg;
     int out;
@@ -191,7 +215,7 @@ int main(void) {
         int length = 0;
         printf("%s", PROMPT);
         
-
+        /*Add job management*/
 
         length = getline(&line, &linecap, stdin);
         int cnt = get_cmd(args, &bg, &out, &pip, length, line);
@@ -202,24 +226,9 @@ int main(void) {
             merge_cmd(&cmd, args, cnt);
         }
         
-        /* Some built-in functions TODO: Do these need to be forked? / Ability to be redirected/piped? */
-        if(strcmp(args[0], "pwd") == 0) {
-            char path[ARG_MAX];
-            if(getcwd(path, ARG_MAX) != NULL) {
-                printf("%s\n", path);
-                continue;
-            }
-        } else if(strcmp(args[0], "cd") == 0) {
-            chdir(args[1]);
-            continue;
-        } else if(strcmp(args[0], "exit") == 0) {
-            exit(0);
-        } else if(strcmp(args[0], "fg") == 0) {
-            pid_t pid = jobs[atoi(args[1]) + 1].pid;
-            waitpid(pid, NULL, 0);
-        } else if(strcmp(args[0], "jobs") == 0) { 
-           list_jobs();
-        } else {
+        int ran = check_exec_built_in(args);
+        if(ran) continue;        
+        else {
             execute(args, cnt, bg, out, pip, cmd);
             if(bg) {
                 free(cmd);
